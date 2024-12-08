1e371a52d84e7fe1db733eedb1c06a8c35eb521d
lemiregm <gabrielle.lemire@outlook.com>
2024-12-07 20:39:38 -0800

    implementing vision logic to user options. add hidden option to see whole map

--- dungon/dungeon_adventure.py
+++ dungon/dungeon_adventure.py
@@ -111,56 +111,46 @@ class DungeonAdventure:
         else:
             return True
 
-    def room_vision(self, num_rm_view = 8):
+    def room_vision(self, vision_potion=False, num_rm_view=8):
         """Prints a visual representation of the nearest
         num_rm_view (defaults to 8) rooms surrounding current room. Implementation of Vision potion
         which is used to allow user to see eight rooms surrounding current room and current room.
         location in maze may cause less than num_rm_view (defaults to 8) to be displayed."""
 
-        view_around = round(num_rm_view/2)
+        if vision_potion is True:
+            # set distance on map around current room to print. currently is 4 in either direction
+            view_around = round(num_rm_view / 2)
 
-        minx, miny = self.current_room.x - view_around, self.current_room.y - view_around
-        maxx, maxy = self.current_room.x + view_around, self.current_room.y + view_around
-        minx = max(minx, 0)  #cutoff at 0
-        miny = max(miny, 0)  # cutoff at 0
+            minx, miny = self.current_room.x - view_around, self.current_room.y - view_around
+            maxx, maxy = self.current_room.x + view_around, self.current_room.y + view_around
+            minx = max(minx, 0)  # remove negatives
+            miny = max(miny, 0)  # remove negatives
 
-        # delete after. helps for troubleshooting
-        print(f"The view in the grid between: bottom left ({minx}, {miny}), \n "
-              f"bottom right ({minx}, {maxy}), \n"
-              f"top left ({maxx}, {miny}), \n"
-              f"top right ({maxx}, {maxy}):")
+        else:
+            miny, maxy = 0, self.dungeon._map_height
+            minx, maxx = 0, self.dungeon._map_width
 
+        # create a list of rooms to include in our visual based on min and max x and y above
         print_rooms = []
         for room in self.dungeon.rooms:
             if minx <= room.x <= maxx and miny <= room.y <= maxy:
                 print_rooms.append(room)
-                # print(f"adding {room} to print out list")
-
-        # delete after. helps for troubleshooting
-        print(f"create visual for following room locations:")
-        for room in print_rooms:
-            print(f"({room.x}, {room.y}) to see \n{room.__str__()}")
-        ## TO DO will still need to print the tunnels after
 
         # print one row at a time. all together will create grid.
-        for print_row in range(miny, maxy + 1): #reverse because grid has high y values above
+        for print_row in range(miny, maxy + 1):
             # each x-coordinate value of 1 (width) gets 3 spaces.
             # each row in dungeon has lines because of stars above and below
-            row_str1 = "   " * (maxx-minx+1)
-            row_str2 = "   " * (maxx-minx+1)
-            row_str3 = "   " * (maxx-minx+1)
-            for room in print_rooms: #check all the rooms to print
-                if room.y == print_row: #if they are in the row
-                    rm_str = room.__str__().replace("\n", "") #remove \n. makes count hard and changes spacing.
-                    row_str1 = f'{row_str1[:(room.x*3)]}{rm_str[0:3]}{row_str1[(room.x*3+3) + 1:]}'
-                    row_str2 = f'{row_str2[:(room.x*3)]}{rm_str[3:6]}{row_str2[(room.x*3+3) + 1:]}'
-                    row_str3 = f'{row_str3[:(room.x*3)]}{rm_str[6:9]}{row_str3[(room.x*3+3) + 1:]}'
+            row_str1 = "   " * (maxx - minx + 1)
+            row_str2 = "   " * (maxx - minx + 1)
+            row_str3 = "   " * (maxx - minx + 1)
+            for room in print_rooms:  # check all the rooms to print
+                if room.y == print_row:  # if they are in the row
+                    # remove \n. makes count hard and changes spacing.
+                    rm_str = room.__str__().replace("\n", "")
+                    # and splice the current row strings (three lines per print_row)
+                    row_str1 = f'{row_str1[:(room.x * 3)]}{rm_str[0:3]}{row_str1[(room.x * 3 + 3) + 1:]}'
+                    row_str2 = f'{row_str2[:(room.x * 3)]}{rm_str[3:6]}{row_str2[(room.x * 3 + 3) + 1:]}'
+                    row_str3 = f'{row_str3[:(room.x * 3)]}{rm_str[6:9]}{row_str3[(room.x * 3 + 3) + 1:]}'
             print(row_str1)
             print(row_str2)
             print(row_str3)
-        #     full_print.append(row_str, "")
-        #
-        # print("printing full")
-        # for dict in full_print:
-        #     print(dict.items())
-
--- main.py
+++ main.py
@@ -18,13 +18,33 @@ def main():
             new_room = d.move_rooms()
             if new_room:
                 d.check_room_content()
+
         elif action == 2:
-            print("HEALTH POTION NOT IMPLEMENTED")
+            """Implement Health Potion. They can increase their health if they have
+            Health Potion in their inventory."""
+            if len(d.adventurer.inventory.get("health potion")) != 0:
+                print("HEALTH POTION NOT IMPLEMENTED")
+                d.adventurer.inventory["health potion"].pop(0)
+            else:
+                print("Nice try! You can't use a Health Potion unless it is in your inventory!\n")
+
         elif action == 3:
-            print("VISION POTION NOT IMPLEMENTED")
-            d.room_vision()
+            """Implement Vision Potion. They can see the dungeon a specified 
+            height and width around the current room if they have Vision Potion
+            in their inventory."""
+            if len(d.adventurer.inventory.get("vision potion")) != 0: #ie if list is not empty
+                # if they have the potion, print visual and remove potion
+                d.room_vision(vision_potion=True)
+                d.adventurer.inventory["vision potion"].pop(0) #removes first vision potion in list
+            else:
+                print("Nice try! You can't use a Vision Potion unless it is in your inventory!\n")
+
         elif action == 4:
             d.adventurer.player_status()
+
+        elif action == 10:
+            """See the whole dungeon map."""
+            d.room_vision(vision_potion=False)
 
 
 if __name__ == "__main__":
